fddf8e74cbc04833238cb79d66fb9aca3ff78f38
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2024-05-09 19:54:51 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from skbuild import setup
+
+import sys
+_cmake_args = []
+
+setup(
+    name="NGS-myassembling",
+    version="1.0.0",
+    author="NGSolve",
+    license="MIT",
+    cmake_args=_cmake_args
+)
